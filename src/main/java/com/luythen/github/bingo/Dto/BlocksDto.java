49013d977a774136e1fe6bdc6baed5daa3153b82
Luythen <andenklasson@gmail.com>
2026-01-14 01:24:24 +0100

    make json file easly readable

---
+++ src/main/java/com/luythen/github/bingo/Dto/BlocksDto.java
@@ -0,0 +1,17 @@
+package com.luythen.github.bingo.Dto;
+
+import java.util.ArrayList;
+
+public class BlocksDto {
+
+    private ArrayList<ItemDto> blocks;
+
+    public ArrayList<ItemDto> getBlocks() {
+        return blocks;
+    }
+
+    public void setBlocks(ArrayList<ItemDto> blocks) {
+        this.blocks = blocks;
+    }
+    
+}
---
+++ src/main/java/com/luythen/github/bingo/Dto/ItemDto.java
@@ -0,0 +1,20 @@
+package com.luythen.github.bingo.Dto;
+
+public class ItemDto {
+
+    private String ItemType;
+    private String ItemImg;
+    public String getItemType() {
+        return ItemType;
+    }
+    public void setItemType(String itemType) {
+        ItemType = itemType;
+    }
+    public String getItemImg() {
+        return ItemImg;
+    }
+    public void setItemImg(String itemImg) {
+        ItemImg = itemImg;
+    }
+    
+}
